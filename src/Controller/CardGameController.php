e4314971acaf85f3f3fca68bb0946db879775b9d
LuDar01 <luda22@student.bth.se>
2024-11-12 22:04:46 +0100

    test file

--- src/Controller/CardGameController.php
+++ src/Controller/CardGameController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Card\Card;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,6 +14,41 @@ class CardGameController extends AbstractController
     public function home(): Response
     {
         return $this->render('card.html.twig');
+    }
+
+    #[Route("/game/pig/roll", name: "test_roll_dice")]
+    public function testRollDice(): Response
+    {
+        $die = new Card();
+
+        $data = [
+            "dice" => $die->roll(),
+            "diceString" => $die->getAsString(),
+        ];
+
+        return $this->render('roll.html.twig', $data);
+    }
+
+    #[Route("/game/pig/roll/{num<\d+>}", name: "test_roll_num_dices")]
+    public function testRollDices(int $num): Response
+    {
+        if ($num > 99) {
+            throw new \Exception("Can not roll more than 99 dices!");
+        }
+
+        $diceRoll = [];
+        for ($i = 1; $i <= $num; $i++) {
+            $die = new Card();
+            $die->roll();
+            $diceRoll[] = $die->getAsString();
+        }
+
+        $data = [
+            "num_dices" => count($diceRoll),
+            "diceRoll" => $diceRoll,
+        ];
+
+        return $this->render('roll-many.html.twig', $data);
     }
 
 }
